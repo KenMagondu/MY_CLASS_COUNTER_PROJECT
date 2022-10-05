73576b7f1e67693e182b453ce17ccf30b782ddde
KenMagondu <98582209+KenMagondu@users.noreply.github.com>
2022-10-05 16:45:20 +0300

    Add files via upload

---
+++ WEB DEVELOPMENT/index.js
@@ -0,0 +1,34 @@
+//HOME SCREEN 
+let count_home=0
+ let homescreen=document.getElementById("HOME_SCREEN")
+ 
+ function add1(){
+     count_home=count_home+1
+     homescreen.innerText=count_home
+ }
+ function add2(){
+     count_home=count_home+2
+     homescreen.innerText=count_home
+     
+ }
+ function add3(){
+    count_home=count_home+3
+    homescreen.innerText=count_home
+ }
+ 
+ 
+ //code for guest screen
+ let count_guest=0
+ let guestScreen=document.getElementById("GUEST_SCREEN")
+ function guestadd1(){
+     count_guest= count_guest+1
+     guestScreen.innerText=count_guest
+ }
+ function guestadd2(){
+      count_guest= count_guest+2
+       guestScreen.innerText=count_guest
+ }
+ function guestadd3(){
+      count_guest= count_guest+3
+       guestScreen.innerText=count_guest
+ }
